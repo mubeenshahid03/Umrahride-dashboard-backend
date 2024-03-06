4fa89067d1b539d983d42660e6b5cf6c10aab08d
mubeenshahid03 <91826576+mubeenshahid03@users.noreply.github.com>
2024-03-06 12:15:28 +0500

    fix booking model in backend

--- Model/Booking.js
+++ Model/Booking.js
@@ -10,6 +10,10 @@ const bookingSchema= new mongoose.Schema({
         type: String,
         required: true,
     },
+    timepicker: {
+        type: String,
+        required: true,
+    },
     flightnumber: {
         type: String,
         //required: true,
